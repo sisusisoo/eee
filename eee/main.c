6bace138b3c665a0c817908f2c85b2313e794aac
sisusisoo <khc97622@naver.com>
2023-03-09 10:41:05 +0900

    Initial Commit

---
+++ eee/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  eee
+//
+//  Created by 강현철 on 2023/03/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
